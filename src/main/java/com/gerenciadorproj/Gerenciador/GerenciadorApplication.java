9d82100a6e9f8e62c6da30f8f87ee30c7e468f84
Ryan Okonski <90875580+RyanOkonski@users.noreply.github.com>
2025-02-21 13:46:05 -0300

    Init
    Signed-off-by: Ryan Okonski <90875580+RyanOkonski@users.noreply.github.com>

---
+++ src/main/java/com/gerenciadorproj/Gerenciador/GerenciadorApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciadorproj.Gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+
+}
---
+++ src/test/java/com/gerenciadorproj/Gerenciador/GerenciadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciadorproj.Gerenciador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
